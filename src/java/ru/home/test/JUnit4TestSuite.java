ef748a957aa71cb8d263f3f6d96789ffdfb86008
smarkin <primatvko@gmail.com>
2016-02-23 23:26:46 +0300

    [*] небольшие изменения.

--- src/java/ru/home/test/JUnit4TestSuite.java
+++ src/java/ru/home/test/JUnit4TestSuite.java
@@ -1,13 +1,15 @@
-package ru.home;
+package ru.home.test;
 
+import org.junit.runner.JUnitCore;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import ru.home.test.ChromeDriverTest;
 
 /**
  * Created by smarkin on 16.02.2016.
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ChromeDriverTest.class})
+@Suite.SuiteClasses({TestRunnerTest.class})
 public class JUnit4TestSuite {
 
 }
