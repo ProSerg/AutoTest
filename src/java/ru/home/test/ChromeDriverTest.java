9d87c06fa4f9d8d1315bb6c97af02fef5f08d380
smarkin <primatvko@gmail.com>
2016-03-13 18:13:11 +0300

    [*] Добавлены параметры для теста.

--- src/java/ru/home/test/ChromeDriverTest.java
+++ src/java/ru/home/test/ChromeDriverTest.java
@@ -1,31 +1,52 @@
 package ru.home.test;
 
-import junit.framework.TestCase;
-import org.jboss.netty.handler.codec.embedder.EncoderEmbedder;
 import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import ru.home.exCore.*;;
+import ru.home.common.ConfigValue;
+import ru.home.common.Locators;
+import ru.home.common.TestParameters;
+import ru.home.exCore.*;
+import ru.home.objects.LogInForm;;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-
+@RunWith(Parameterized.class)
 public class ChromeDriverTest extends Assert {
-    final static String chromdriver = "E:\\workspace\\libexec\\chromedriver.exe"; //FIXME
+    final static String chromdriver = ConfigValue.getValue("chromdriver"); //FIXME
     private static ChromeDriverService service;
     public static WebDriver driver;
     static File chrome;
-
+    private static ConfigValue config;
+    private static TestParameters parameters;
+    private String inputLogin,inputPass,expectedResult;
     Button Enter;
-    Input  lineSearch;
+    LogInForm loginForm;
 
-    public ChromeDriverTest () {}
+    public  ChromeDriverTest(String name,String pass, String expected) {
+        this.inputLogin = name;
+        this.inputPass = pass;
+        this.expectedResult = expected;
+    }
+
+    // {input, result }
+   @Parameterized.Parameters
+    public static Collection primeNumbers() throws IOException {
+        parameters = new TestParameters("/parameters.properties");
+        return parameters.getValue("Enter.testLight"); //testMain
+    }
+
+
 
     @BeforeClass
     public static void AndStartSecreatervice() throws Exception {
@@ -41,9 +62,16 @@ public class ChromeDriverTest extends Assert {
 
     @Before
     public void setUp() throws Exception {
-        System.out.println("BeforeClass SetUp");
+        System.out.println();
+        System.out.println("BeforeClass SetUp!!!");
         driver = new RemoteWebDriver(service.getUrl(),
                 DesiredCapabilities.chrome());
+
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
+
+        Enter = new Button(driver,HTMLElement.SearchBy.CSS_SELECTOR,Locators.get("LoginPage.loginButton"));
+        loginForm = new LogInForm(driver,HTMLElement.SearchBy.CSS_SELECTOR,Locators.get("LoginPage.Form"));
     }
 
 
@@ -67,12 +95,15 @@ public class ChromeDriverTest extends Assert {
         service.stop();
     }
 
+
     @Test
     public void testBase() throws InterruptedException {
-        driver.get("http://socialkey.ru/");
-        Enter = new Button(driver,HTMLElement.SearchBy.CSS_SELECTOR,"body > header > div > div.menu.pull-right > a.btn.btn-success.log-in");
+        driver.get( ConfigValue.getValue("site") );
         System.out.println("Bool:"+Enter.isEnabled());
         Enter.click();
+        loginForm.fill(inputLogin,inputPass);
+        loginForm.Submit();
+        assertEquals(expectedResult, loginForm.getLog());
     }
 
 }
