9af0d0c797dded4254814533fa2c03db135799f1
smarkin <primatvko@gmail.com>
2016-03-16 18:44:03 +0300

    [+] тест вкладки Рекомендуемое

--- src/java/ru/home/test/PriceTest.java
+++ src/java/ru/home/test/PriceTest.java
@@ -1,5 +1,6 @@
 package ru.home.test;
 
+import com.sun.xml.internal.bind.v2.model.core.ID;
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -115,18 +116,113 @@ public class PriceTest extends Assert {
 
     @Test
     public void testTabRecommend() throws InterruptedException {
+        ItemContainer container;
         pricePage.goTo();
         tabRecommend.goTo();
 
-       // priceCart.findCartElements();
         tabRecommend.findBoxElements();
 
+        //Проверка титул пустой корзины
         assertEquals(Locators.getValue("Price.Cart.Tittle.Empty"),   priceCart.getActualTittle());
         assertTrue(priceCart.isDisplayedEmpty() );
         assertFalse(priceCart.isDisplayedFull() );
 
-        tabRecommend.multiClickBoxButton(TabRecommend.LeftButton, 10);
-        tabRecommend.multiClickBoxButton(TabRecommend.RightButton, 10);
+        container = priceCart.findCartElements();
+        System.out.println(container.toString());
+        System.out.println("Coast: " + priceCart.getTotalCoast() );
+        //Проверка содержимой пустой корзины
+        assertTrue(container.isEmpty());
+
+        //проверка изменения статуса кнопки покупки "левой" до и после нажатия
+        assertTrue(tabRecommend.isButtonEnabled(TabRecommend.IdButton.Left) );
+        tabRecommend.multiClickBoxButton(TabRecommend.IdButton.Left, 1);
+        assertFalse(tabRecommend.isButtonEnabled(TabRecommend.IdButton.Left) );
+        //
+
+        container.refresh();
+        //Проверка содержимой не пустой корзины
+        assertTrue(!container.isEmpty());
+
+  //      System.out.println(tabRecommend.getBoxTittle(TabRecommend.IdButton.Left));
+  //      System.out.println(container);
+
+        // сверка название товара с титулом из корзины
+        assertTrue(container.contains(tabRecommend.getBoxTittle(TabRecommend.IdButton.Left)));
+        //TODO сверка цены
+        //
+
+        //проверка изменения статуса кнопки покупки "правой" до и после нажатия
+        assertTrue(tabRecommend.isButtonEnabled(TabRecommend.IdButton.Right) );
+        tabRecommend.multiClickBoxButton(TabRecommend.IdButton.Right, 1);
+        assertFalse(tabRecommend.isButtonEnabled(TabRecommend.IdButton.Right) );
+        //
+
+        container.refresh();
+
+//        System.out.println("RButton:"+ tabRecommend.getBoxButton(TabRecommend.IdButton.Right).isEnabled() );
+//        System.out.println(container);
+
+        // сверка название товара с титулом из корзины
+        assertTrue(container.contains("Неделя доступа к базе резюме"));
+        //TODO сверка цены
+        //
+
+        //проверка суммы
+        System.out.println("Coast: " + priceCart.getTotalCoast());
+        System.out.println("Calculate: " + priceCart.calcPrice());
+        assertTrue(priceCart.isCoast());
+        //Проверка титула не пустой корзины
+        assertEquals(Locators.getValue("Price.Cart.Tittle.Full"), priceCart.getActualTittle());
+        assertFalse(priceCart.isDisplayedEmpty() );
+        assertTrue(priceCart.isDisplayedFull() );
+
+        //Проверка колличество покупок в корзине
+        assertEquals(2, container.size());
+
+        //System.out.println(container.toString());
+
+        //Удаление одной покупки в корзине
+        container.removeItem("Неделя доступа к базе резюме");
+        assertTrue(container.refresh());
+        assertTrue(tabRecommend.getBoxButton(TabRecommend.IdButton.Right).isEnabled() );
+
+        //Проверка колличество покупок в корзине
+        assertEquals(1, container.size());
+        System.out.println(container.toString());
+
+        //Удаление второй покупки в корзине
+        container.removeItem("Вакансия Стандарт+");
+        container.refresh();
+        assertTrue(tabRecommend.getBoxButton(TabRecommend.IdButton.Left).isEnabled() );
+
+
+        assertTrue(container.isEmpty());
+
+        //END
+/*
+        assertTrue(tabRecommend.isButtonEnabled(TabRecommend.IdButton.Left) );
+        tabRecommend.multiClickBoxButton(TabRecommend.IdButton.Left, 1);
+        assertFalse(tabRecommend.isButtonEnabled(TabRecommend.IdButton.Left) );
+
+        container.refresh();
+        assertTrue(!container.isEmpty());
+
+        System.out.println(tabRecommend.getBoxTittle(TabRecommend.IdButton.Left));
+        System.out.println(container);
+
+        assertTrue(container.contains(tabRecommend.getBoxTittle(TabRecommend.IdButton.Left)));
+
+        assertTrue(tabRecommend.isButtonEnabled(TabRecommend.IdButton.Right) );
+        tabRecommend.multiClickBoxButton(TabRecommend.IdButton.Right, 1);
+        assertFalse(tabRecommend.isButtonEnabled(TabRecommend.IdButton.Right) );
+
+
+        container.refresh();
+
+        System.out.println("RButton:"+ tabRecommend.getBoxButton(TabRecommend.IdButton.Right).isEnabled() );
+        System.out.println(container);
+
+        assertTrue(container.contains("Неделя доступа к базе резюме"));
 
         System.out.println("Coast: " + priceCart.getTotalCoast());
 
@@ -134,10 +230,25 @@ public class PriceTest extends Assert {
         assertFalse(priceCart.isDisplayedEmpty() );
         assertTrue(priceCart.isDisplayedFull() );
 
-       // assertEquals(priceCart.getDriver().getTitle(), priceCart.clickPayment().getName());
-        priceCart.findCartElements();
+        assertEquals(2, container.size());
 
-        Thread.sleep(3000);
+        System.out.println(container.toString());
+
+        container.removeItem("Неделя доступа к базе резюме");
+        assertTrue(container.refresh());
+        //Thread.sleep(1000);
+        assertTrue(tabRecommend.getBoxButton(TabRecommend.IdButton.Right).isEnabled() );
+
+        assertEquals(1, container.size());
+        System.out.println(container.toString());
+
+        container.removeItem("Вакансия Стандарт+");
+        container.refresh();
+        //Thread.sleep(1000);
+        assertTrue(tabRecommend.getBoxButton(TabRecommend.IdButton.Left).isEnabled() );
+
+        assertEquals(0, container.size());
+*/
     }
 
 }
